551202bcb9193004ee851621324e5d38338a4637
Duong Pham Qui <duongpq@nal.vn>
2023-12-25 15:42:43 +0700

    Remove Request required

--- src/core/decorator/__init__.py
+++ src/core/decorator/__init__.py
@@ -1,4 +1,3 @@
-from core.decorator.request_validation import request_validation
 from core.decorator.response_jsonify import response_jsonify
 
-__all__ = ['request_validation', 'response_jsonify']
+__all__ = ['response_jsonify']
